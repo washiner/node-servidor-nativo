0dc49d18e6d38d1334d586126ccd858cd77cc4c4
Washiner Takeuchi <washiner@gmail.com>
2023-03-01 11:04:50 -0400

    codigo final

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # node-servidor-nativo
 
-<h1>
+<h3>
 1 - Essa aula e para entender como funciona o modulo http no node e como ele tem nativo vamos fazer ele funcionar.
 2 - O node ele tem varios modulos como fs por ex ele pode escrever e deletar arquivos
 3 - temos outros modulos para fazer isso que fizemos aqui com modulo http original do node mas claro tem bibliotecas mais recomendadas como nodemon e tambem o express mas antes de estudar eles vamos entender os fundamentos
 
-</h1>
+</h3>
--- servidor.js
+++ servidor.js
@@ -1,1 +1,18 @@
-let http = require('http')
+let http = require('http')  // cria uma variavel chamando modulo http nativo do node
+
+http.createServer(function(request, response){
+    response.end("Hello World voce esta logado no servidor")
+}).listen(3000)   // abre um servidor - ouve o servir com listen - parametro e a porta
+
+console.log("o servidor esta rodando")  // menssagem pra gente visualizar que o servidor ta rodando
+
+
+
+
+
+// - NOTAS
+// - a funcao create server tem uma funcao de callback que recebe dois parametros request e response
+// - o response e uma resposta para o usuario na tela
+// - o end e para enviar uma menssagem
+// - quando utilizar o nodemon nao vamos precisar ficar reiniciando servidor
+// - quando utilizar o express sera mais resumido o codigo tb
